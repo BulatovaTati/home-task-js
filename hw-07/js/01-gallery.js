db885045dcd9fcf36f0793f26aebc9c29329d852
BulatovaTati <102695106+BulatovaTati@users.noreply.github.com>
2022-07-30 11:46:17 +0300

    Update 01-gallery.js

--- hw-07/js/01-gallery.js
+++ hw-07/js/01-gallery.js
@@ -1,30 +1,49 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
- 
-const box = document.querySelector('.gallery');
-box.addEventListener('click', onBoxClick);
- 
-const elem = galleryItems.map(createItems).join('');
-// 1
-function createItems ({preview, original, description})  {
-    return box.insertAdjacentHTML("beforeend", ` 
+import { galleryItems } from "./gallery-items.js";
+
+const box = document.querySelector(".gallery");
+
+box.addEventListener("click", onBoxClick);
+galleryItems.map(createItems).join("");
+
+function createItems({ preview, original, description }) {
+  return box.insertAdjacentHTML(
+    "beforeend",
+    `
           <a class="gallery__link" href="${original}">
           <img class="gallery__image"
           src="${preview}"
-          data-source="${original}" 
-          alt="${description}" 
+          data-source="${original}"
+          alt="${description}"
           />
         </a>
-    ` );
- 
+    `
+  );
 }
-//  2
-function onBoxClick(evt) {
-    evt.preventDefault();
-}
-//  2
-const instance = basicLightbox.create(
-    document.querySelector
-)
 
-instance.show()
+function onBoxClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  instance.element().querySelector("img").src = evt.target.dataset.source;
+  instance.show();
+}
+
+// 4
+const instance = basicLightbox.create(`<img class="modal__image" src=""/>`, {
+  onShow: (instance) => {
+    window.addEventListener("keydown", onEscClick);
+  },
+  onClose: (instance) => {
+    window.removeEventListener("keydown", onEscClick);
+  },
+});
+
+function onEscClick(evt) {
+  if (evt.key === "Escape") {
+    instance.close();
+    return;
+  }
+}
