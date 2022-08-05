b970ebcba848023055071bdf81f2ce783334badb
BulatovaTati <102695106+BulatovaTati@users.noreply.github.com>
2022-08-05 20:12:38 +0300

    hw-08 task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
