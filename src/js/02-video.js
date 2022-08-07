48c94886a06c7858d31079d713efecf89dc45a5c
BulatovaTati <102695106+BulatovaTati@users.noreply.github.com>
2022-08-07 14:25:03 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,32 +2,24 @@ import Player from '@vimeo/player';
 import Throttle from 'lodash.throttle';
 const STORAGE_KEY = 'videoplayer-current-time';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// player.on('timeupdate', Throttle(onVideoPlay, 1000));
-
-// function onVideoPlay({ seconds }) {
-//   localStorage.setItem(STORAGE_KEY, seconds);
-// }
-
-// player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate', Throttle(onVideoPlay, 1000));
-
 function onVideoPlay({ seconds }) {
   try {
     localStorage.setItem(STORAGE_KEY, seconds);
   } catch (error) {
-    console.log('error', error);
+    console.log('Error:', error.message);
   }
 }
 
-try {
-  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
-} catch (error) {
-  console.log('error', error);
-}
+(function onPlayerLoad() {
+  try {
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).catch(error => {
+      console.log('Error:', error.message);
+    });
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
+})();
