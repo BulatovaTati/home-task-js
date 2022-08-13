2fa43e5a59fa569214de4bdd3788d9e2240771cc
BulatovaTati <kohodatat@gmail.com>
2022-08-13 14:05:40 +0300

    add test timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,4 +3,51 @@ import flatpickr from 'flatpickr';
 import Notiflix from 'notiflix';
 
 // Дополнительный импорт стилей
-import 'flatpickr/dist/flatpickr.min.css';
+// import 'flatpickr/dist/flatpickr.min.css';
+
+// const refs = {
+//   timer: document.querySelector('.timer'),
+// };
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
+
+const millisecondsInSecond = 1000;
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const hoursInDay = 24;
+
+const timer = targetDate => {
+  setInterval(() => {
+    const delta = new Date(targetDate) - new Date();
+
+    const seconds = Math.floor(
+      (delta / millisecondsInSecond) % secondsInMinute
+    );
+    const minutes = Math.floor(
+      (delta / (millisecondsInSecond * secondsInMinute)) % minutesInHour
+    );
+
+    const hours = Math.floor(
+      (delta / (millisecondsInSecond * secondsInMinute * minutesInHour)) %
+        hoursInDay
+    );
+    const days = Math.floor(
+      delta /
+        (millisecondsInSecond * secondsInMinute * minutesInHour * hoursInDay)
+    );
+    const timerFormat = `${days}:${hours}:${minutes}:${seconds}`;
+    renderTimer(timerFormat);
+  }, 1000);
+};
+
+const renderTimer = string => {
+  document.querySelector('span').innerText = string;
+};
+timer(new Date('2022/08/15'));
