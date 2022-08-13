3dc0ec96b7b5735529d5e6b0a5fd5522ca1ced08
BulatovaTati <kohodatat@gmail.com>
2022-08-13 17:10:28 +0300

    do task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start'),
+  stopBtn: document.querySelector('button[data-stop'),
+  body: document.querySelector('body'),
+};
+let intervalId = null;
 
+refs.startBtn.addEventListener('click', onButtonStart);
+refs.stopBtn.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function onButtonStop() {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
