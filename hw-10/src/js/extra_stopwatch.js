4c8c37d550b41a8357956b04a70cbbd2e68734bd
BulatovaTati <kohodatat@gmail.com>
2022-08-19 11:58:35 +0300

    add hw-09

---
+++ hw-10/src/js/extra_stopwatch.js
@@ -0,0 +1,80 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start'),
+  stopBtn: document.querySelector('button[data-stop'),
+  resetBtn: document.querySelector('button[data-reset'),
+  span: document.querySelector('span'),
+};
+
+const millisecondsInSecond = 1000;
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const hoursInDay = 24;
+
+let intervalId = null;
+let delta;
+let initialDate;
+
+const renderTimer = string => {
+  refs.span.innerText = string;
+};
+
+const start = () => {
+  initialDate = delta ?? new Date();
+
+  intervalId = setInterval(() => {
+    const delta = new Date() - initialDate;
+
+    const milliseconds = Math.floor(delta % millisecondsInSecond);
+    const seconds = Math.floor(
+      (delta / millisecondsInSecond) % secondsInMinute
+    );
+    const minutes = Math.floor(
+      (delta / (millisecondsInSecond * secondsInMinute)) % minutesInHour
+    );
+
+    const hours = Math.floor(
+      (delta / (millisecondsInSecond * secondsInMinute * minutesInHour)) %
+        hoursInDay
+    );
+    const days = Math.floor(
+      delta /
+        (millisecondsInSecond * secondsInMinute * minutesInHour * hoursInDay)
+    );
+
+    const UISeconds =
+      `${seconds}`.length === 2 ? seconds : `${seconds}`.padStart(2, '0');
+    const UIMinutes = minutes.toString().length === 2 ? minutes : `0${minutes}`;
+    const UIHours = `${hours}`.length === 2 ? hours : `0${hours}`;
+
+    const timerFormat = `${days}:${UIHours}:${UIMinutes}:${UISeconds}:${milliseconds}`;
+
+    if (intervalId !== null) {
+      let pauseTime = delta + Date.now();
+      renderTimer(pauseTime);
+      console.log('pauseTime: ', pauseTime);
+    }
+
+    renderTimer(timerFormat);
+
+    refs.startBtn.disabled = true;
+    refs.resetBtn.disabled = true;
+  }, 0);
+};
+
+const startStop = () => {};
+
+const stop = () => {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+  refs.resetBtn.disabled = false;
+};
+
+const reset = () => {
+  refs.span.innerText = '';
+};
+
+refs.startBtn.addEventListener('click', start);
+refs.startBtn.addEventListener('click', startStop);
+
+refs.stopBtn.addEventListener('click', stop);
+refs.resetBtn.addEventListener('click', reset);
