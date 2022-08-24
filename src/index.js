97aa4f671a4d5f72b34336186e4fc55b031b1a18
BulatovaTati <kohodatat@gmail.com>
2022-08-24 11:56:32 +0300

    add dependencies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
