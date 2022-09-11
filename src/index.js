c8907e5689a08dbcecf96edcf5e5e6cc50c77af1
BulatovaTati <kohodatat@gmail.com>
2022-09-11 12:51:18 +0300

    add test task

--- src/index.js
+++ src/index.js
@@ -88,17 +88,97 @@ function domMarkup(resFin) {
   gallery.insertAdjacentHTML('beforeend', markupGallery(resFin));
 }
 
-// window.addEventListener('scroll', smoothScrollPage);
+// // window.addEventListener('scroll', smoothScrollPage);
 
-// async function smoothScrollPage() {
-//   if (
-//     document.documentElement.scrollHeight -
-//       document.documentElement.scrollTop <=
-//     document.documentElement.clientHeight + 1
-//   ) {
-//     try {
-//       apiRequest();
-//     } catch (error) {
-//       console.log('Line 77', error);
-//     }
+// // async function smoothScrollPage() {
+// //   if (
+// //     document.documentElement.scrollHeight -
+// //       document.documentElement.scrollTop <=
+// //     document.documentElement.clientHeight + 1
+// //   ) {
+// //     try {
+// //       apiRequest();
+// //     } catch (error) {
+// //       console.log('Line 77', error);
+// //     }
+// //   }
+// // Напиши клас Client який створює об'єкт /
+// /з властивостями login email
+//   / / Оголоси приватні властивості #login #email,
+// //доступ до яких зроби через геттер та сеттер login email
+
+// class Client {
+//   #login;
+//   #email;
+//   constructor({ login, email }) {
+//     this.#email = email;
+//     this.#login = login;
 //   }
+
+//   get login() {
+//     return this.#login;
+//   }
+//   set login(NewLogin) {
+//     this.#login = NewLogin;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+//   set email(NewEmail) {
+//     this.#email = NewEmail;
+//   }
+// }
+
+// const NewClient = new Client({ login: 'fhfhhfh', email: 'dffkjhif' });
+// console.log('NewClient: ', NewClient);
+// console.log(NewClient.email);
+// console.log(NewClient.login);
+// console.log((NewClient.email = 'dd12'));
+// console.log(NewClient.email);
+
+//Напиши клас Notes, який управляє колекцією нотаток у
+//властивості items.
+//Нотатка - це об'єкт із властивостями text priority
+//Додай класу статичну властивість Priopity,
+//у якій зберігатиметься об'єкт із пріоритетами.
+//Додай методи addNote(note), removeNote(text)
+//updatePriority(text, newPriority)
+
+// class Notes {
+//   static Priopity = [];
+//   constructor({ text, priority }) {
+//     this.items = text;
+//     this.priopity = priority;
+//   }
+//   addNote(note) {
+//     this.items.push(note);
+//   }
+//   updatePriority(text, newPriority) {}
+// }
+
+// const note = new Notes({ text: 'jfdhfhdsfhasifhihsaf', priopity: [] });
+
+// note.updatePriority({ text: '111', newPriority: 'base1' });
+// note.addNote({ text: '222', newPriority: 'base2' });
+// note.addNote({ text: '333', newPriority: 'base3' });
+// note.addNote('hshfusdhf');
+// console.log('note: ', note);
+
+// 09/09/2022 test task
+// Делегування подій
+// 1. Коли користувач клікає на будь-яку комірку
+// із таблиці, потрібно її зробити активною - добавити клас.active
+// 3. В кожному рядку кожній третій комірці задавати клас .active-third
+// 2. Коли користувач клікає на іншу комірку, вона робиться активною, а всі інші стають неактивними
+// 3. Після перезавантаження сторінки активна комірка зберігається
+
+// const board = document.querySelector('.board');
+// console.log('board: ', board);
+
+// board.addEventListener('click', evt => {
+//   console.dir(evt.currentTarget);
+//   if (evt.target.tagName === 'TD') {
+//     evt.target.classList.toggle('active');
+//   }
+// });
--- src/js/api/fetch.js
+++ src/js/api/fetch.js
@@ -4,11 +4,9 @@ export default class Gallery {
   #API_KEY = '29527006-ad6e7c34d6702116665004a30';
   #BASE_URL = 'https://pixabay.com/api/';
 
-  constructor() {
-    this.page = 1;
-    this.searchQuery = '';
-    this.per_page = 40;
-  }
+  page = 1;
+  searchQuery = '';
+  per_page = 40;
 
   async fetchPictures() {
     const url = await axios.get(this.#BASE_URL, {
